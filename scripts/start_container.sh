eb11a8de2415e61f8a498937f9809804ce0e6609
Saminder Singh <91252787+SaminderSingh@users.noreply.github.com>
2025-07-01 20:51:23 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push sammy34719/simple-python-flask-app:tagname
+docker push sammy34719/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sammy34719/simple-python-flask-app:tagname
+docker run -d -p 5000:5000 sammy34719/simple-python-flask-app
